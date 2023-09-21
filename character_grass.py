0a5999c89b88ae8623a81d88dc69225e114b28d9
gamejoon <gamejoon0325@naver.com>
2023-09-21 10:13:46 +0900

    원운동 완성

--- character_grass.py
+++ character_grass.py
@@ -1,8 +1,9 @@
 from pico2d import *
+import os
+import math
 
 open_canvas()
 
-import os
 os.chdir("D:\\2DGP\\Drill04")
 
 grass = load_image("grass.png")
@@ -10,7 +11,19 @@ character = load_image("character.png")
 
 def run_circle():
     print("CIRCLE")
-    pass
+    
+    # 일단 그림을 그려라
+    
+    cx, cy, r = 400, 300, 200
+
+    for deg in range(0, 360, 5):
+        x = cx + r * math.cos(math.radians(deg))
+        y = cy + r * math.sin(math.radians(deg))
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+    
+        delay(0.01)
 
 def run_rectangle():
     print("RECTANGLE")
