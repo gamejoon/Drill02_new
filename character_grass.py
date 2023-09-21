47b933be0a74420e0a930c6d3f357be784fd449e
gamejoon <gamejoon0325@naver.com>
2023-09-21 10:03:22 +0900

    create character_grass.py

---
+++ character_grass.py
@@ -0,0 +1,22 @@
+from pico2d import *
+
+open_canvas()
+
+import os
+os.chdir("D:\\2DGP\\Drill04")
+
+grass = load_image("grass.png")
+character = load_image("character.png")
+
+def run_circle():
+    print("CIRCLE")
+    pass
+
+def run_rectangle():
+    print("RECTANGLE")
+    pass
+
+while (True):
+    run_circle()
+    run_rectangle()
+    
